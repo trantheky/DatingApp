2aae7a07507a4fb1b3cf0a7b9242558fad67c9ab
Ky Tran <trantheky1987@gmail.com>
2019-10-26 17:32:47 +0700

    End of Section 14

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Pagination, PaginationResult } from './../../_models/pagination';
 import { AlertifyService } from '../../_services/alertify.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/User';
@@ -11,6 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  user:  User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
+  pagination: Pagination;
 
   constructor(private userService: UserService, 
               private alertify: AlertifyService,
@@ -18,16 +23,45 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit() { 
     this.route.data.subscribe(data => {
-      this.users = data['users'];
-   });
-   }
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
 
   loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, 
+                              this.userParams)
+                    .subscribe((res: PaginationResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  /*loadUsers() {
     this.userService.getUsers().subscribe((users: User[]) => {
       this.users = users;
     }, error => {
       this.alertify.error(error);
     });
-  }
+  }*/
 
 }
